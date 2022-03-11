b963db9e533a354403f4a0c1f44f0269e5d35cc4
Charles Sarichith <charsari337@ed.amdsb.ca>
2022-03-11 10:01:30 -0500

    Add project files.

---
+++ TronDisc/MenuScreen.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TronDisc
+{
+    partial class MenuScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.instructionsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("OCR A Extended", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.titleLabel.Location = new System.Drawing.Point(0, 38);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(400, 23);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Tron Disc Duel";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Black;
+            this.startButton.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(146, 157);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(105, 55);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "startButton";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // instructionsButton
+            // 
+            this.instructionsButton.BackColor = System.Drawing.Color.Black;
+            this.instructionsButton.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionsButton.ForeColor = System.Drawing.Color.White;
+            this.instructionsButton.Location = new System.Drawing.Point(146, 233);
+            this.instructionsButton.Name = "instructionsButton";
+            this.instructionsButton.Size = new System.Drawing.Size(105, 55);
+            this.instructionsButton.TabIndex = 2;
+            this.instructionsButton.Text = "instructionsButton";
+            this.instructionsButton.UseVisualStyleBackColor = false;
+            this.instructionsButton.Click += new System.EventHandler(this.instructionsButton_Click);
+            // 
+            // MenuScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.instructionsButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "MenuScreen";
+            this.Size = new System.Drawing.Size(400, 400);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button instructionsButton;
+    }
+}
---
+++ TronDisc/MenuScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TronDisc
+{
+    public partial class MenuScreen : UserControl
+    {
+        public MenuScreen()
+        {
+            InitializeComponent();
+            startButton.Text = "Start";
+            instructionsButton.Text = "Instructions";
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void instructionsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
